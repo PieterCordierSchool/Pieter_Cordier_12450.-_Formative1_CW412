f3ea13e66c0933110225dec792ce8bc7e2912d19
PieterCordierSchool <20231284@ctucareer.co.za>
2024-01-09 14:44:23 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".title2", {
+    strings: ["Full Time Student,", "Freelance Web Developer,", "Web Designer"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
+//getting all required elements
